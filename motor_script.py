7dab346b99fe127b8a2b30833c3c2b42504e59a5
Tihnan Azfar Ahmed <tihnanazfar@gmail.com>
2026-04-26 12:02:58 -0400

    Update motor_script.py

--- motor_script.py
+++ motor_script.py
@@ -4,9 +4,11 @@ import time
 
 motor1 = Motor(1,2) 
 # insert the pin numbers as connected 
+# change this based on the pins we use 
 
 motor1._clockwise() 
 # turns motor clockwise 
+
 
 
 time.sleep(7) 
@@ -15,13 +17,14 @@ time.sleep(7)
 motor1._anticlockwise()
 # turns other way
 
-time.sleep(3) 
+time.sleep(5) 
 
 motor1._clockwise() 
 
 # clockwise again
 
-time.sleep(3)
+time.sleep(10)
+# 10 second delay
 
 motor1._stop() 
 # now the motor stops 
